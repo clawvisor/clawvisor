a8b832f8c137560be306b306e018abdbe01297a9
Eric Levine <eric@levine.tech>
2026-05-08 21:11:43 -0700

    Bind Vite dev server on IPv4

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,7 @@ const backendURL = `http://localhost:${backendPort}`
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
     allowedHosts: true,
     proxy: {
